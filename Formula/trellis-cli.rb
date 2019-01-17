6051dadb4e587348ed8b4b378495fa6a293794b2
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-16 23:56:45 -0500

    Brew formula update for trellis version v0.1.2

--- Formula/trellis-cli.rb
+++ Formula/trellis-cli.rb
@@ -1,9 +1,9 @@
 class TrellisCli < Formula
   desc "A CLI to manage Trellis projects"
   homepage "https://roots.io/trellis"
-  url "https://github.com/roots/trellis-cli/releases/download/v0.1.1/trellis_0.1.1_Darwin_x86_64.tar.gz"
-  version "0.1.1"
-  sha256 "c61da88e57cd29a9943608a6f9ce73c2653532fc8a5577c717f8f48e39b4d843"
+  url "https://github.com/roots/trellis-cli/releases/download/v0.1.2/trellis_0.1.2_Darwin_x86_64.tar.gz"
+  version "0.1.2"
+  sha256 "1da94f70353924c9f7756bf3454bf949ea6d0cec5218a4240b14cb5fdf285c0c"
 
   def install
     bin.install "trellis"
