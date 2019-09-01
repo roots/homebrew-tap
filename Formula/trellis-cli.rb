177665893ac404744d372d36813a1ae68e77b73a
goreleaserbot <goreleaser@carlosbecker.com>
2019-09-01 16:19:42 -0400

    Brew formula update for trellis version v0.5.0

--- Formula/trellis-cli.rb
+++ Formula/trellis-cli.rb
@@ -2,15 +2,17 @@
 class TrellisCli < Formula
   desc "A CLI to manage Trellis projects"
   homepage "https://roots.io/trellis"
-  version "0.4.0"
+  version "0.5.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/roots/trellis-cli/releases/download/v0.4.0/trellis_0.4.0_Darwin_x86_64.tar.gz"
-    sha256 "3172cdd98ad2af60d37bfb8dfab4b1fffe4daba2af0ae211b1b3eeca5dbc05c9"
+    url "https://github.com/roots/trellis-cli/releases/download/v0.5.0/trellis_0.5.0_Darwin_x86_64.tar.gz"
+    sha256 "469f8b04ecbb30d2ad206ab674fc8fe17ce0fd76d10185b1bea9ac596afeda54"
   elsif OS.linux?
-    url "https://github.com/roots/trellis-cli/releases/download/v0.4.0/trellis_0.4.0_Linux_x86_64.tar.gz"
-    sha256 "daabf5bb7673e8df0b5ce94abc3ce700e4411a3b990c2aad767eb40100052087"
+    if Hardware::CPU.intel?
+      url "https://github.com/roots/trellis-cli/releases/download/v0.5.0/trellis_0.5.0_Linux_x86_64.tar.gz"
+      sha256 "e9a54a47143af5a203b01ae308030aa4eb219c83eec87edbf3796201c1072eeb"
+    end
   end
 
   def install
