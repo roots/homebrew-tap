97dd7c138f7e2d67b410f80d958c941a72ae0420
goreleaserbot <goreleaser@carlosbecker.com>
2021-11-01 10:18:20 -0400

    Brew formula update for trellis version v1.0.0

--- Formula/trellis-cli.rb
+++ Formula/trellis-cli.rb
@@ -10,7 +10,15 @@ class TrellisCli < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/roots/trellis-cli/releases/download/v1.0.0/trellis_1.0.0_Darwin_x86_64.tar.gz"
-      sha256 "e8ce7454201f5b5305ec7e58a2dcd2fe447d1c07808e70fb88fa57f5fc9701b0"
+      sha256 "fc3face49c26b8e2b59eb9b897606716b378917eed06913ef0c03ee9f523b9ca"
+
+      def install
+        bin.install "trellis"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/roots/trellis-cli/releases/download/v1.0.0/trellis_1.0.0_Darwin_arm64.tar.gz"
+      sha256 "7bb22c55ef87061aef172fe4db0c39bec27015210ee2d1d9ac805739be9b355b"
 
       def install
         bin.install "trellis"
@@ -21,7 +29,15 @@ class TrellisCli < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/roots/trellis-cli/releases/download/v1.0.0/trellis_1.0.0_Linux_x86_64.tar.gz"
-      sha256 "de375e4c54c67dbe1bc2c63de25ba4880436eb8d449e2997b2114f5a51ae1ce4"
+      sha256 "5e0536828003f5b45c2c83dc5af7157edf4b6bbf7b12c162ef015583b762ea4b"
+
+      def install
+        bin.install "trellis"
+      end
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/roots/trellis-cli/releases/download/v1.0.0/trellis_1.0.0_Linux_arm64.tar.gz"
+      sha256 "40a188b01900d49128662abe917349d4f07110ec015fecfcb350ed4fe7524424"
 
       def install
         bin.install "trellis"
